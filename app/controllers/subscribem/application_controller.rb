065facaf4dbf3975e5dceddfce9f35265cd43408
thaddavis <thaddadavis@gmail.com>
2016-05-25 14:48:24 -0400

    up to 2.5

--- app/controllers/subscribem/application_controller.rb
+++ app/controllers/subscribem/application_controller.rb
@@ -1,5 +1,25 @@
 module Subscribem
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
+
+    def current_account
+      if user_signed_in?
+        @current_account ||= env["warden"].user(:scope => :account)
+      end
+    end
+    helper_method :current_account
+
+    def current_user
+      if user_signed_in?
+        @current_user ||= env["warden"].user(:scope => :user)
+      end
+    end
+    helper_method :current_user
+
+    def user_signed_in?
+      env["warden"].authenticated?(:user)
+    end
+    helper_method :user_signed_in?
+
   end
 end
